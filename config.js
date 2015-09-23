f3ae5bb72ce9a45322787e2c48ce27584a35bfc7
Shuaiyi Liu <davidliu.thu@gmail.com>
2015-09-23 16:41:40 +0800

    implemented model-mongodb and tested

---
+++ config.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var path = require('path');
+
+
+module.exports = {
+  port: '8989',
+  mongodb: {
+    url: 'mongodb://127.0.0.1:27017/test',
+    collection: 'cd_reviews'
+  }
+};
+
+
